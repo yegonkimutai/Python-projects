6d9897ac509340c6ebec0604db601986713c1620
Brian Yrgon <yegonkimutai.99@gmail.com>
2023-10-15 08:22:26 +0300

    Add the game functionality

--- pig/project1.py
+++ pig/project1.py
@@ -8,11 +8,39 @@ def roll():
     return roll
  
 while True:
-    players = input('Enter Number of players(1-4): ')
+    players = input('Enter Number of players(2 - 4): ')
     if players.isdigit():
         players = int(players)
         if 2 <= players <= 4:
-            print('Must be btwn 1 - 4 players.')
             break
         else:
-            print('Invalid choice, try again')
+            print('Must be btwn 2 - 4 players.')
+    else:
+        print('Invalid choice, try again')
+
+max_score = 50
+player_scores = [0 for _ in range(players)]
+
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print('\nPlayer', player_idx + 1, 'turn has just begun!\n')
+        current_score = 0
+        
+        while True: 
+            should_roll = input('Would you like to roll(y)?')
+            if should_roll.lower() != 'y':
+                break
+
+            value = roll()
+            if value == 1:
+                print('You rolled a 1! Turn done!')
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print('You rolled a:', value)
+
+            print('Your score is:', current_score)
+
+        player_scores[player_idx] += current_score
+        print('Your total score is:', player_scores[player_idx])
